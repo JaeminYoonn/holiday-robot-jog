68ae43c031a09c837df8690b18a77a8ebb72ec49
jaemin.yoon <jaemin.yoon@holiday-robotics.com>
2025-05-19 16:25:56 +0900

    add docker

---
+++ .devcontainer/postCreateCommand.sh
@@ -0,0 +1,12 @@
+# holiday ros2
+cd ${HOLIDAY_ROS_ROOT}/src
+#git clone -b main git@github.com:Holiday-Robot/holiday-robotics-library.git
+#git clone -b main git@github.com:Holiday-Robot/holiday-ros2-interface.git
+
+cd ${HOLIDAY_ROS_ROOT}
+colcon build --symlink-install
+echo "
+# Holiday ROS2 environment
+source /holiday/ros2/install/setup.zsh" >> ~/.zshrc
+
+nvidia-smi
